a44b020d0c16a962aa371f624e3db8c13dde89e0
Daniil Bagaev <daniil.bagaev@yahoo.com>
2022-12-15 06:43:56 +0500

    v.0.0.9
    Add new features

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const
     { config } = require('./config/app.config.json'),
     jubatus = require(config.jubatus);
 
+//jubatus.test();
 //jubatus.log('test', {filename: 'index.log'});
+//jubatus.task('pug', './src/**/*.pug', './build/');
 jubatus.git();
